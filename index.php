4f54e7dbdf77f44646aafda716ad73ea282ce397
Your Name <you@example.com>
2019-12-13 06:01:23 +0100

    tempsave

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 <div class="text">
 <h1>Hackers Cardgame</h1>
-Never Play "Red Team" help out with "Blue Team" and save the World<br><br>
+Never Play "<font color=red>Red Team</font>" help out with "<font color=blue>Blue Team</font>" and save the World<br><br>
 please respect the <a href="../../LICENSE">license</a>, People like Julian Assange, Fefe, Frank Rieger, @Paniq & Silvia, Gunnar Kaiser, Dan Kaminsky, John Hunter, (world peace game), Richard Stallman, Linux Torvalds, Tschökk Sparrow &amp;&amp; Stefan Bader &amp; Co are excluded from the 1978 Rule in my license, and even if he passed away Joseph Weizenbaum would be excluded from the 1978 rule too. And all members of the Demoscene that have an valid entry on scene-ftp
 <br>if you think you should be excluded from the "1978 and younger"-Rule you can contact me on twitter @FailDef<br><br>
 People that work for the NDB are explicitely excluded and not allowed to read, scan, track, modify my cards, since i guess they sent me to mental institition again after i posted #NDB<=>#Psychiatrie, but if you think you *E* are one of the good guys at NDB, talk to me.
